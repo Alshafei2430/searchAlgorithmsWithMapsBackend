1102ee68858fa872e1075e65b7748c8773821707
Alshafei2430 <alshafei.2430@gmail.com>
2021-11-17 22:46:43 +0200

    test master push

--- api.py
+++ api.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return 'Welcome to the Algorithms Api'
+
+@app.route('/test')
+def testMasterPush():
+    return 'this is a push from the master pranch'
